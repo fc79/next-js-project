5883c9d1455b41108ae5153471d0ee20cd76df05
fatemeh <gholizadefatemeh37@gmail.com>
2024-03-05 16:50:27 +0330

    added midleware page

---
+++ middleware.js
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request){
+    // return NextResponse.redirect(new URL('/quiz', request.url))
+    return NextResponse.rewrite(new URL('/under-update', request.url))
+}
+// export const config = {
+//     matcher: "/about",
+// }
+export const config = {
+    matcher : "/((?!api|_next/static|_next/image|favicon.ico).*)"
+}
